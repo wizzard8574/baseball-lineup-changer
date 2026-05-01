548fced39c85bee07ad1a97ed38f0950b5e80abe
Rich Morris <wizzard@zoomtown.com>
2026-05-01 00:24:34 -0400

    Fix iOS folder submodule

---
+++ ios/Lineup Changer/Lineup Changer/Lineup_Changer.swift
@@ -0,0 +1,17 @@
+//
+//  Baseball_Positions_and_LineupApp.swift
+//  Baseball Positions and Lineup
+//
+//  Created by Rich Morris on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct Lineup_Changer: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/Lineup Changer/Lineup ChangerTests/Lineup_Changer.swift
@@ -0,0 +1,19 @@
+//
+//  Baseball_Positions_and_LineupTests.swift
+//  Baseball Positions and LineupTests
+//
+//  Created by Rich Morris on 4/28/26.
+//
+
+import Testing
+@testable import Lineup_Changer
+
+struct Baseball_Positions_and_LineupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
